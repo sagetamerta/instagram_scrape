10bfb05d995d48a37aa8a81c1193fb7c45c11b96
sagetamerta <38368716+sagetamerta@users.noreply.github.com>
2023-06-03 10:53:08 +0800

    up

--- scripts/following_followers.py
+++ scripts/following_followers.py
@@ -5,13 +5,11 @@ from dotenv import load_dotenv
 from datetime import datetime
 from tzlocal import get_localzone
 
-
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime):
             return obj.isoformat()
         return super().default(obj)
-
 
 # Get the local time zone
 timezone = get_localzone()
